f54a906ebabcc4c46d4bb6de2bacd715fedd5fcf
Sumanth Tirumalasetti <st185178@candescent.com>
2025-12-30 22:30:13 +0530

    Testing arrays with indexing

--- 06-testing_arrys.sh
+++ 06-testing_arrys.sh
@@ -2,8 +2,8 @@
 
 MOVIES=("Pushpa" "rrr" "devara")
 
-echo "First movie is ${MOVIES[0]}"
-echo "Second movie is ${MOVIES[0]}"
-echo "Third movie is ${MOVIES[0]}"
+echo "First movie is :: ${MOVIES[0]}"
+echo "Second movie is :: ${MOVIES[0]}"
+echo "Third movie is :: ${MOVIES[0]}"
 
-echo "All movies are ${MOVIES[@]}"
+echo "All movies are :: ${MOVIES[@]}"
