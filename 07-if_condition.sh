bc0dc4179f39b5e193ea7f79e1519a553c6175e8
Sumanth Tirumalasetti <st185178@candescent.com>
2025-12-30 22:36:20 +0530

    if condition

--- 07-if_condition.sh
+++ 07-if_condition.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -eq 0 ]
 then
-    echo "Given number is : $NUMBER"
+    echo "Given number is equal to 0"
 else
-    echo "Given number is : $NUMBER"
+    echo "Given number is not equal to 0"
 fi
