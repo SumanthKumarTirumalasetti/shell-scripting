e23e274b394be855556886d05099fb469fc2c96e
Sumanth Tirumalasetti <st185178@candescent.com>
2025-12-30 21:59:43 +0530

    Printing the varaibles through command line as arguments

---
+++ 03-argment_variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hi $PERSON1"
+echo "$PERSON1:: How are you?"
+echo "$PERSON2:: I am doing good"
+
+# Giving the varibales values through commad line
