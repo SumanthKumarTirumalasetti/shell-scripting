2459529c4b69a556d12b4665a870a3371c0bf782
Sumanth Tirumalasetti <st185178@candescent.com>
2026-01-01 12:10:12 +0530

    installing mysql

--- 08-installing_mssql.sh
+++ 08-installing_mssql.sh
@@ -7,4 +7,11 @@ then
     echo "user must have supervison privilages to execute this script"
 fi
 
-#dnf install mysql -y
+dnf install mysql-server -y
+
+if [ $? -ne 0 ]
+then
+    echo "Mysql is already installed"
+else
+    dnf install mysql-server -y
+fi
