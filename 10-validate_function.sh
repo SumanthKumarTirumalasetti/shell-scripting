9a32016bbffc57274f7e5e85255a09105fcd24a2
Sumanth Tirumalasetti <st185178@candescent.com>
2026-01-01 13:00:32 +0530

    Using validate FUNCTION to replace the repeated code

---
+++ 10-validate_function.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE() {
+
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 FAILURE"
+        exit1
+    else
+        echo "$2 SUCCESS"
+    fi
+
+}
+
+
+if [ $USERID -ne 0 ]
+then
+    else "ERROR:: You must have sudo access to execute this script"
+    exit 1
+fi
+
+dnf list mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql-server -y
+    VALIDATE() $? "Installing Mysql"
+else
+    echo "Mysql is already installed"
+fi
+
+dnf list git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE() $? "Installing Git"
+else
+    echo "Git is already installed"
+fi
