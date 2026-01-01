64f9d9d96ed474986d1a28c79b161b24d15e405a
Sumanth Tirumalasetti <st185178@candescent.com>
2026-01-01 14:12:09 +0530

    redirecting logs

---
+++ 12-logs.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+O="\e[33m"
+N="\e[0m"
+
+mkdir -p /var/log/expense-logs
+
+LOGS_DIR="/var/log/expense-logs"
+FILENAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOGFILENAME=$("$LOGS_DIR/$FILENAME-$LOGFILENAME.log")
+
+
+VALIDATE() {
+
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 .. $R FAILURE $N"
+        exit1
+    else
+        echo -e "$2 .. $G SUCCESS $N"
+    fi
+
+}
+
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit1
+fi
+
+dnf list installed mysql &>> $LOGFILENAME
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql-server -y &>> $LOGFILENAME
+    VALIDATE $? "Installing Mysql"
+else
+    echo -e  "Mysql is already $O installed $N"
+fi
+
+dnf list installed git &>> $LOGFILENAME
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y &>> $LOGFILENAME
+    VALIDATE $? "Installing Git"
+else
+    echo -e "Git is already $O installed $N"
+fi
+
+dnf list installed ansible &>> $LOGFILENAME
+
+if [ $? -ne 0 ]
+then
+    dnf install ansible -y &>> $LOGFILENAME
+    VALIDATE $? "Installing Ansible"
+else
+    echo -e "Ansible is already $O installed $N"
+fi
