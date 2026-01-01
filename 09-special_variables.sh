b8aeda5c652297e2255b3e8d7dcb8aa1df4b572a
Sumanth Tirumalasetti <st185178@candescent.com>
2026-01-01 12:03:27 +0530

    printing special variables

--- 09-special_variables.sh
+++ 09-special_variables.sh
@@ -3,9 +3,9 @@ echo $1 $2 $3 $4 $5 $6 $7 $8 $9 #positional aruguments
 echo $@ # List of all arguments
 echo $# # Number of all arguments
 echo $0 # Script name
-echo $pwd # present working directory
-echo $user # present working user
-echo $home # Present user home directory
+echo $PWD # present working directory
+echo $USER # present working user
+echo $HOME # Present user home directory
 echo $$ # Process id of current script
 echo $! # Process id of last command in background
 echo $? # Exit status of last command
