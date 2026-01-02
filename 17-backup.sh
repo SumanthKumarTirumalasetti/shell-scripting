075d4bf8ae48c344523e490a7c9de29bf5575110
Sumanth Tirumalasetti <st185178@candescent.com>
2026-01-02 22:01:30 +0530

    archving logs

--- 17-backup.sh
+++ 17-backup.sh
@@ -9,21 +9,21 @@ N="\e[0m"
 
 SOURCE_DIRECTORY=$1
 DESTINATION_DIRECOTRY=$2
-NUMBEROFDAYS=${3:14}
+NUMBEROFDAYS=${3:-14}
 
 mkdir -p /home/ec2-user/app-logs
 mkdir -p /home/ec2-user/destination-logs
-
-
-if [ $# -lt 2]
-then
-    USAGE
-fi
 
 USAGE() {
     echo -e "$R USAGE:: $N backup <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
     exit 1
 }
+
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
+
 
 if [ ! -d $SOURCE_DIRECTORY ]
 then
