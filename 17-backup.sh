cf5b42bd02a6aea33c4aafc55b6704d969eb95a3
Sumanth Tirumalasetti <st185178@candescent.com>
2026-01-02 21:48:22 +0530

    archving logs

---
+++ 17-backup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+O="\e[33m"
+N="\e[0m"
+
+SOURCE_DIRECTORY=$1
+DESTINATION_DIRECOTRY=$2
+NUMBEROFDAYS=${3:14}
+
+
+if [ $# -lt 2]
+then
+    USAGE
+fi
+
+USAGE() {
+    echo -e "$R USAGE:: $N backup <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    exit 1
+}
+
+if [ -d $SOURCE_DIRECTORY ]
+then
+    USAGE
+fi
+
+if [ -d $DESTINATION_DIRECOTRY ]
+then
+    USAGE
+fi
+
