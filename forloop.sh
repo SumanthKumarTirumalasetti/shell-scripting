bba27ed01ae72e644ea34ed27a2ee352dca2ac75
Sumanth Tirumalasetti <st185178@candescent.com>
2026-01-03 06:35:00 +0530

    craeting logs using for loop

--- forloop.sh
+++ forloop.sh
@@ -3,7 +3,8 @@
 LOGSDIR=/home/ec2-user/for-logs
 FILESTOCREATE=("app.log" "bap.log" "console.log" "managment.log" "tomme.log" "sequce.log" "tomee.xml")
 
-for i in $FILESTOCREATE
+for i in "$FILESTOCREATE[@]"
 do
+    cd $LOGS_DIR
     touch -d 20140101 $i
 done
